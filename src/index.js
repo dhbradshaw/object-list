27e31a18297fbfc4cec6f7613f920cbd3875198d
Douglas H. Bradshaw <dhbradshaw@gmail.com>
2017-05-10 13:50:04 -0500

    Add binnedArray, first, and last functions for processing lists of objects.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log('hi')
+function binnedArray(arr, keyFunction = element => element.id) {
+  const binned = {}
+  const length = arr.length
+  for (let i = 0; i < length; i += 1) {
+    const element = arr[i]
+    const key = keyFunction(element)
+    const val = binned[key] || []
+    binned[key] = val.concat([element])
+  }
+  return binned
+}
+
+const first = (arr, discriminator = () => true) => {
+  const length = arr.length
+  for (let i = 0; i < length; i += 1) {
+    const a = arr[i]
+    if (discriminator(a)) {
+      return a
+    }
+  }
+  return undefined
+}
+
+const last = (arr, discriminator = () => true) => {
+  for (let i = arr.length - 1; i >= 0; i -= 1) {
+    const a = arr[i]
+    if (discriminator(a)) {
+      return a
+    }
+  }
+  return undefined
+}
+
+export {
+  binnedArray,
+  first,
+  last,
+}
